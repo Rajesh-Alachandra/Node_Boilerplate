9a77be9f44da9158813600689fcf9cbb226df97d
Rajesh Alachandra <108992336+Rajesh-Alachandra@users.noreply.github.com>
2024-06-19 23:06:12 +0530

    post

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,16 +1,6 @@
 const productService = require('../services/productService');
 const { errorResponse, validateProductData } = require('../utils/helpers');
 
-exports.postProduct = async (req, res) => {
-    try {
-        validateProductData(req.body);
-        const product = await productService.postProduct(req.user.id, req.body);
-        res.status(201).json(product);
-    } catch (error) {
-        errorResponse(res, 400, error.message);
-    }
-};
-
 
 
 exports.postProduct = async (req, res) => {
